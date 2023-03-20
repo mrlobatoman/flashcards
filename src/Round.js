a816805edad1595fac842ab342063dc9032f13b1
Michael Lobato <mike.lobato.wft@gmail.com>
2023-03-20 11:10:59 -0700

    Add: pseudo code for method

--- src/Round.js
+++ src/Round.js
@@ -5,6 +5,21 @@ class Round {
         this.incorrectGuesses = [],
         this.turns = 0 
     }
+
+    returnCurrentCard() {
+        return this.cardInPlay
+    }
+
+    takeTurn() {
+// call upon the Turn class to take in user guess
+// if guess is incorrect, store the id into this.incorrectGuesses
+// return feedback
+// new instance of card with next card being next in array
+// need to ++ to this.turns anytime function invoked
+
+    }
+
+
 }
 
 module.exports = Round
