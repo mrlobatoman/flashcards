eff365de060b082cf5a57732ad41d05d1bb9a824
Michael Lobato <mike.lobato.wft@gmail.com>
2023-03-19 18:44:25 -0700

    Add: feedback to user informing them if the answer is correct or not

--- src/Turn.js
+++ src/Turn.js
@@ -19,6 +19,14 @@ class Turn {
     evaluateGuess() {
         return this.userGuess === this.currentCard.correctAnswer
     }
+
+    giveFeedback() {
+        if(this.evaluateGuess()) {
+            return `correct!`
+        } else {
+            return `incorrect!`
+        }
+    }
 }
 
 module.exports = Turn
