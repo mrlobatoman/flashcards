d3a07fb1ef8b852b60222c87de0743bf00b4e901
Michael Lobato <mike.lobato.wft@gmail.com>
2023-03-19 18:27:09 -0700

    Add: method that returns the users guess

--- src/Turn.js
+++ src/Turn.js
@@ -3,6 +3,15 @@ class Turn {
         this.userGuess = userGuess,
         this.currentCard = currentCard
     }
+// create a method that will return the users guess when invoked
+// when creating a new instance of turn, the userGuess will be user input and is
+    // assigned to the userGuess property
+
+
+returnGuess() {
+    this.userGuess
+}
+
 }
 
 module.exports = Turn
