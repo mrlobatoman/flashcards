012c14cb23014b9d3da3acfeb4bc965dd29ffb72
Michael Lobato <mike.lobato.wft@gmail.com>
2023-03-20 16:23:50 -0700

    fix: syntax issue and calculate method

--- src/Round.js
+++ src/Round.js
@@ -16,10 +16,17 @@ class Round {
         let turn = new Turn(guess, this.cardInPlay)
         this.turns += 1
         this.cardInPlay = this.deck.cards[this.turns]
-        if(!turn.evaluateGuess) {
+        if(!turn.evaluateGuess()) {
             this.incorrectGuesses.push(this.cardInPlay.id)
         } 
         return turn.giveFeedback()
+    }
+
+    calculatePercentageCorrect() {
+        let deckSize = this.deck.cards.length 
+        let incorrectGuessAmount = this.incorrectGuesses.length
+        let percentage = 100 - (Math.floor((incorrectGuessAmount / deckSize) * 100))
+        return percentage
     }
 }
 
