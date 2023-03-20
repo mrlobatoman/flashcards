554e59270b295287bea1caf2a6da772d85be21bd
Michael Lobato <mike.lobato.wft@gmail.com>
2023-03-20 11:22:16 -0700

    Add: testing file along with imports of needed files and beforeEach

---
+++ test/Round-test.js
@@ -0,0 +1,19 @@
+const chai = require('chai')
+const expect = chai.expect
+
+const Round = require('../src/Round')
+const Turn = require('../src/Turn')
+const Deck = require('../src/Deck')
+const Card = require('../src/Card')
+
+describe('Round', function() {
+    let card1, card2, card3, cards, deck, round
+    beforeEach(() => {
+        card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object')
+        card2 = new Card(2, 'What is a comma-separated list of related values?', ['array', 'object', 'function'], 'array')
+        card3 = new Card(3, 'What type of prototype method directly modifies the existing array?', ['mutator method', 'accessor method', 'iteration method'], 'mutator method')
+        cards = [card1, card2, card3]
+        deck = new Deck(cards)
+        round = new Round(deck)
+    })
+})
