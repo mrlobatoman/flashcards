6824d40d0a0a78524b4164b21f10a161a1901d89
Michael Lobato <mike.lobato.wft@gmail.com>
2023-03-20 17:20:28 -0700

    Add: a method that starts the game

--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,14 @@
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+const Round = require('../src/Round');
 
 class Game {
-  constructor() {}
+  constructor() {
+    this.currentRound
+  }
 
   printMessage(deck, round) {
     console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
@@ -12,6 +17,19 @@ class Game {
 
   printQuestion(round) {
       util.main(round);
+  }
+
+  start(firing) {
+    const cards = []
+    prototypeQuestions.forEach(card => {
+      cards.push(new Card(card.id, card.question, card.answers, card.correctAnswer))
+    })
+    const deck = new Deck(cards)
+    this.currentRound = new Round(deck)
+    if(firing){
+      this.printMessage(deck, this.currentRound)
+      this.printQuestion(this.currentRound)
+    }
   }
 }
 
